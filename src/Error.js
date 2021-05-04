95e79389f14d963912b0f5a258cdb24955d53821
varma <varmaruddraraju@gmail.com>
2021-05-04 20:10:16 +0530

    Fxied 404 error attempt 2

--- src/Error.js
+++ src/Error.js
@@ -9,7 +9,7 @@ const Error = () => {
             </header>
             <p className="fw1 i tc mt4 mt5-l f4 f3-l">Are you looking for one of these?</p>
             <ul className="list tc pl0 w-100 mt5">
-                <li className="dib"><a className="f5 f4-ns link black db pv2 ph3 hover-light-purple" href="/">Home</a></li>
+                <li className="dib"><a className="f5 f4-ns link black db pv2 ph3 hover-light-purple" href="/reactjs">Home</a></li>
             </ul>
         </section>
     );
